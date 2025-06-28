41fde7ef19298b76507164be1f44423f536c573c
Shubham Mohapatra <shubhammohapatra094@gmail.com>
2025-06-28 13:04:34 +0530

    updated frontend(Added Bootstrap)

--- sql-chatbot-frontend/src/index.js
+++ sql-chatbot-frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider, createTheme } from "@mui/material/styles"; // Add this import
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const theme = createTheme(); // You can customize the theme if needed
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
